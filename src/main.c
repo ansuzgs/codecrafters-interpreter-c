7f5b62faaea67482d6ccda1394708e7847e046f6
PGS <ansuz.gs@gmail.com>
2025-01-10 21:26:54 +0100

    multiline errors

--- src/main.c
+++ src/main.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]) {
                 case '/':
                     if (file_contents[i+1] == '/') {
                         while(file_contents[i++] != '\n');
-                        line_counter++;
+                        i--;
                     } else {
                         printf("SLASH / null\n");
                     }
